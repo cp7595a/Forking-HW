21f34c0835c23c89d1e143c612c58416dede0db9
Elaine Esther Oruk Opyene <149006668+elaineoruk@users.noreply.github.com>
2024-03-26 19:12:40 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,7 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+diamonds1 <- as.data.table(diamonds)
+diamonds2 <- diamonds1[cut == "Premium" & price > 7000 & price < 10000,  c("carat", "cut", "price")]
+diamonds2[order(-carat)][1:20]
